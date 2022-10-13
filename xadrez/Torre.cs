f6256c8ba80ca10fa9d93bc86f9a4924a5150b93
Rafael Rossi <rtrafaeltrova@gmail.com>
2022-10-13 20:23:42 -0300

    Implementado metodo de movimentos possiveis da torre

--- xadrez/Torre.cs
+++ xadrez/Torre.cs
@@ -10,6 +10,74 @@ namespace xadrez
 
         }
 
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tab.Peca(pos);
+            return p == null || p.Cor != this.Cor;
+
+        }
+
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] mat = new bool[tab.Linhas, tab.Colunas];
+
+            Posicao pos = new Posicao(0, 0);
+
+            // Acima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while(tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if(tab.Peca(pos) != null && tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+                pos.Linha = pos.Linha - 1;
+            }
+
+            // Abaixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+                pos.Linha = pos.Linha + 1;
+            }
+
+            // Direita
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+                pos.Coluna = pos.Coluna + 1;
+            }
+
+            // Esquerda
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            while (tab.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
+                if (tab.Peca(pos) != null && tab.Peca(pos).Cor != Cor)
+                {
+                    break;
+                }
+
+                pos.Coluna = pos.Coluna - 1;
+            }
+
+            return mat;
+        }
+
         public override string ToString()
         {
             return "T";
